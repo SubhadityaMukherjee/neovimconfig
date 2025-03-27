f2bc98845fb4536182088fa191718d10ff46c3b4
SubhadityaMukherjee <msubhaditya@gmail.com>
2025-03-27 11:22:16 +0100

    backup

--- lua/vimconfig/init.lua
+++ lua/vimconfig/init.lua
@@ -1,6 +1,6 @@
-require("theprimeagen.set")
-require("theprimeagen.remap")
-require("theprimeagen.lazy_init")
+require("vimconfig.set")
+require("vimconfig.remap")
+require("vimconfig.lazy_init")
 
 -- DO.not
 -- DO NOT INCLUDE THIS
